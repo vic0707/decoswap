7937fb05ed4c6baa294f0cc72da9ed68b79788cc
Victor Macias <v.maciascastillo@gmail.com>
2016-11-29 17:14:36 +0100

    added route improvement

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :items do
-    resources :bookings
+  resources :items, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resource :profile, only: [:show, :edit, :update] do
+  	resources :bookings, only: [:edit, :update]
+  end
+
+  resource :dashboard, only: [:show] do
+  	resources :items, only: [:edit, :update]
   end
 
   root to: 'pages#home'
-
 end
