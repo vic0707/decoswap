d4713e781d06d009b31287f09b47a2a81a80ed86
Minh Lam <minh@minh.be>
2016-11-29 11:00:18 +0100

    Validations added

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  validates: :name, presence: true, uniqueness: true
+  validates: :type, presence: true
+  validates: :size, presence: true
+  validates: :price, presence: true
+  validates: :status, presence: true
+
+
+
+
 end
