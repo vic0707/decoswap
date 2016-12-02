2727a4ae141ddbcc86539303ba9af01cc82904ab
Stan Pollet <polletstanislas@gmail.com>
2016-12-02 16:31:39 +0100

    dashboards

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,7 @@ class ProfilesController < ApplicationController
 	def show
 		@user = current_user
     @sum_of_prices = sum_of_prices(@user)
+    @sum_of_items = sum_of_items(@user)
   end
 
 	def edit
@@ -15,8 +16,23 @@ class ProfilesController < ApplicationController
   def sum_of_prices(user)
     sum = 0
     user.bookings.each do |booking|
+      if booking.item.status == "Rent"
       sum += booking.item.price
+      else
+      end
     end
     return sum
   end
+
+  def sum_of_items(user)
+    summ = 0
+    user.bookings.each do |booking|
+      if booking.item.status == "Rent"
+      summ += 1
+      else
+      end
+    end
+    return summ
+  end
+
 end
