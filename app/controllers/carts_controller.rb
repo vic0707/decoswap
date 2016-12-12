adcac2a7aa5e5a19d8d3a56e4bded547c97a399a
Minh Lam <minh@minh.be>
2016-12-12 19:20:25 +0100

    cart fixed

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,8 +3,8 @@ class CartsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  		@order = current_user.current_order
-		@carts = current_user.current_order.carts
+  	@order = current_user.current_order
+    @carts = @order ? @order.carts : []
 	end
 
 	def new
