96195429978accb47932e171d691d238f3520d42
Stan Pollet <polletstanislas@gmail.com>
2016-12-14 17:27:37 +0100

    mailers erased

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,19 +4,4 @@ class UserMailer < ApplicationMailer
   # with the following lookup:
   #
   #   en.user_mailer.welcome.subject
-  default from: 'team.decoswap@gmail.com'
-
-  def welcome(user)
-    @user = user
-
-    mail(to: @user.email, subject: 'Welcome to Le Wagon')
-  end
-
-  def creation_confirmation(booking)
-    @booking = booking
-    mail(
-      to:       @booking.user.email,
-      subject:  "Booking #{@booking.item} created!"
-    )
-  end
 end
